700b5704a155ad88ef7f45ba5b45d4ef30fa95c3
Luis Cerutti <luissantimcerutti@hotmail.com>
2023-05-05 09:44:30 -0300

    first

---
+++ TesteIMC/TestarIMC/CalculoIMCTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TesteIMC;
+
+namespace TestarIMC
+{
+    [TestClass]
+
+    public class CalculoIMCTests
+    {
+        [TestMethod]
+        public void CalcularIMC()
+        {
+            double peso = 110;
+            double altura = 1.79;
+
+            var resultadoIMC = Operacoes.CalcularIMC(peso, altura);
+
+            Assert.AreEqual(34.33, resultadoIMC);
+
+        }
+    }
+}
